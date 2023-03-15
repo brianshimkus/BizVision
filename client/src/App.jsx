c692e441274c33dd8ab39c52c971c5630f5e0f54
Brian Shimkus <brianshimkus1@gmail.com>
2023-03-14 20:49:52 -0500

    Initial Redux implementation for theme styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
+import { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+import { themeSettings } from './theme'
+
 export default function App() {
+	const mode = useSelector((state) => state.global.mode)
+	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
 	return (
 		<div>
-			<h1>BizVision</h1>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+			</ThemeProvider>
 		</div>
 	)
 }
