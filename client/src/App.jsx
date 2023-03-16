14f54539d66c967a93b5d46e5531f370defa3ad2
Brian Shimkus <brianshimkus1@gmail.com>
2023-03-15 23:50:43 -0500

    Setup routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { CssBaseline, ThemeProvider } from '@mui/material'
 import { createTheme } from '@mui/material/styles'
 import { useMemo } from 'react'
 import { useSelector } from 'react-redux'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Dashboard from './scenes/dashboard'
+import Layout from './scenes/layout'
 import { themeSettings } from './theme'
 
 export default function App() {
@@ -9,9 +12,17 @@ export default function App() {
 	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
 	return (
 		<div>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-			</ThemeProvider>
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Routes>
+						<Route element={<Layout />}>
+							<Route path='/' element={<Navigate to='/dashboard' replace />} />
+							<Route path='/dashboard' element={<Dashboard />} />
+						</Route>
+					</Routes>
+				</ThemeProvider>
+			</BrowserRouter>
 		</div>
 	)
 }
