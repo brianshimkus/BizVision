03aec6dc6bcf5c0423d7e6aec3b9f01c7bc0e8e0
Brian Shimkus <brianshimkus1@gmail.com>
2023-03-16 19:45:01 -0500

    Built out right side of Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -52,6 +52,20 @@ export default function Navbar() {
 						</IconButton>
 					</FlexBetween>
 				</FlexBetween>
+
+				{/* Right Side */}
+				<FlexBetween>
+					<IconButton onClick={() => dispatch(setMode())}>
+						{theme.palette.mode === 'dark' ? (
+							<DarkModeOutlined sx={{ fontSize: '25px' }} />
+						) : (
+							<LightModeOutlined sx={{ fontSize: '25px' }} />
+						)}
+					</IconButton>
+					<IconButton>
+						<SettingsOutlined sx={{ fontSize: '25px' }} />
+					</IconButton>
+				</FlexBetween>
 			</Toolbar>
 		</AppBar>
 	)
