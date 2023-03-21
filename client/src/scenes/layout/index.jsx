fca848733f4ac982863985ccb8ee5d346a10134d
Brian Shimkus <brianshimkus1@gmail.com>
2023-03-20 23:35:46 -0500

    Get user data

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -3,14 +3,19 @@ import { Outlet } from 'react-router-dom'
 import Sidebar from '../../components/Sidebar'
 import Navbar from '../../components/Navbar'
 import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useGetUserQuery } from '../../state/api'
 
 export default function Layout() {
 	const isNonMobile = useMediaQuery('(min-width: 600px)')
 	const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+	const userId = useSelector((state) => state.global.userId)
+	const { data } = useGetUserQuery(userId)
 
 	return (
 		<Box display={isNonMobile ? 'flex' : 'block'} width='100%' height='100%'>
 			<Sidebar
+				user={data || {}}
 				isNonMobile={isNonMobile}
 				drawerWidth='250px'
 				isSidebarOpen={isSidebarOpen}
@@ -18,6 +23,7 @@ export default function Layout() {
 			/>
 			<Box flexGrow={1}>
 				<Navbar
+					user={data || {}}
 					isSidebarOpen={isSidebarOpen}
 					setIsSidebarOpen={setIsSidebarOpen}
 				/>
