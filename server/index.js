fe3d6232752d5f3369b812d3e886d9debdaa8ad2
Brian Shimkus <brianshimkus1@gmail.com>
2023-03-21 23:33:44 -0500

    Added products and product stats to database

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ import salesRoutes from './routes/sales.js'
 
 // data imports
 import User from './models/User.js'
-import { dataUser } from './data/index.js'
+import Product from './models/Product.js'
+import ProductStat from './models/ProductStat.js'
+import { dataUser, dataProduct, dataProductStat } from './data/index.js'
 
 // Configuration
 dotenv.config()
@@ -41,6 +43,9 @@ mongoose
 	.then(() => {
 		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
 
+		// Only add data to database once
+		// Product.insertMany(dataProduct)
+		// ProductStat.insertMany(dataProductStat)
 		// User.insertMany(dataUser)
 	})
 	.catch((error) => {
