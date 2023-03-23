28a9c92c8e497fd429f7f845cc62444b344772ea
Brian Shimkus <brianshimkus1@gmail.com>
2023-03-23 00:32:50 -0500

    Product page scaffold

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -15,9 +15,26 @@ import { useGetProductsQuery } from '../../state/api'
 import Header from '../../components/Header'
 
 export default function Products() {
+	const { data, isLoading } = useGetProductsQuery()
+	const isNonMobile = useMediaQuery('(min-width: 1000px)')
+
 	return (
-		<Box>
+		<Box m='1.5rem 2.5rem'>
 			<Header title='PRODUCTS' subtitle='See your list of products.' />
+			{data || !isLoading ? (
+				<Box
+					mt='20px'
+					display='grid'
+					gridTemplateColumns='repeat(4, minmax(0, 1fr))'
+					justifyContent='space-between'
+					rowGap='20px'
+					columnGap='1.33%'
+					sx={{
+						'& > div': { gridColumn: isNonMobile ? undefined : 'span 4' },
+					}}></Box>
+			) : (
+				<>Loading...</>
+			)}
 		</Box>
 	)
 }
