6e64038844cf89a88ee3511483a9bce6aedb2966
Brian Shimkus <brianshimkus1@gmail.com>
2023-03-29 23:19:34 -0500

    Built out geography page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Layout from './scenes/layout'
 import { themeSettings } from './theme'
 import Customers from './scenes/customers'
 import Transactions from './scenes/transactions'
+import Geography from './scenes/geography'
 
 export default function App() {
 	const mode = useSelector((state) => state.global.mode)
@@ -25,6 +26,7 @@ export default function App() {
 							<Route path='/products' element={<Products />} />
 							<Route path='/customers' element={<Customers />} />
 							<Route path='/transactions' element={<Transactions />} />
+							<Route path='/geography' element={<Geography />} />
 						</Route>
 					</Routes>
 				</ThemeProvider>
